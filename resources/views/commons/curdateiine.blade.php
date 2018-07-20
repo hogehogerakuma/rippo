5634b3c40b5e6b24facf9c3e4bf5bda74de90528
riko.matsunaga <riko.matsunaga@rakuten.com>
2018-07-20 04:42:42 +0000

    20dejulio

--- resources/views/commons/curdateiine.blade.php
+++ resources/views/commons/curdateiine.blade.php
@@ -1,9 +1,8 @@
-
-
 <style>
-@import url('https://fonts.googleapis.com/css?family=Caveat|Dancing+Script|Gaegu|Great+Vibes|Lobster+Two');
+@import url('https://fonts.googleapis.com/css?family=Caveat%7CDancing+Script%7CGaegu%7CGreat+Vibes%7CLobster+Two');
 </style>
 <div class style="font-family: 'Merienda', cursive;">
+
 <?php
             date_default_timezone_set('Asia/Tokyo');
             $now_month =  (int)date("m");
@@ -14,6 +13,7 @@
 $popopo = App\Report::whereDate('created_at', DB::raw('CURDATE()'))->where('user_id', $user->id)->get();
         
         if (isset ($popopo) && count($popopo)>0 ) {
+
             $dashitaka = 'Your Daily Report was already submitted';
         }
         
@@ -26,9 +26,11 @@ $favoriters = DB::table('user_favorite')
         ->join('reports', 'reports.id', '=', 'user_favorite.report_id')
         ->join('users', 'users.id', '=', 'user_favorite.user_id')
         ->whereDay('reports.created_at' , $now_date)
-        ->where('reports.user_id', $user->id)
-        ->select('users.username')
+        // ->where('reports.user_id', $user->id)
+        // ->select('users.username')
         ->get();
+        
+        
       $fav_label = '';
       foreach($favoriters as $f) {
           if($f !== end($favoriters)){
@@ -42,20 +44,51 @@ $favoriters = DB::table('user_favorite')
         $fav_label = substr( $fav_label,0,strlen( $fav_label)-2);
     }
 
-    if(count($favoriters)>0){
-   print '<h3>Your Report   Get'. $favorited .'<span class= "glyphicon glyphicon-heart"></span>  from'.$fav_label.'</h3>' ;
-}
-
-?>
-</div>
+   if (count($favoriters) == 1){
+       $like = "like";
+   }else{
+       $like = "likes";
+   }
 
    if(count($favoriters)>0) {
-       $goukei = $user->username.", You got ".$favorited." likes on your Daily Report ";
+       $goukei = " ".$favorited." ".$like."  "."on Daily Report";
     }
     else{
         $goukei = "";
     }
+    
+    $numfeedtoday = DB::table('comments')
+        ->join('reports', 'reports.id', '=', 'comments.report_id')
+        ->whereDay('reports.created_at', $now_date)
+        ->where('reports.user_id', $user->id)
+        ->count();
+      
+     if ($numfeedtoday == 1){
+       $comment = "comment";
+   }else{
+       $comment = "comments";
+   }
+   
+    if ($numfeedtoday == 0 ){
+        $goukeifeed = "";
+    } else{
+        $goukeifeed = $numfeedtoday." ".$comment;
+    }
 ?>
-<h2><?php print $goukei; ?></h2>
-</div>
 
+<h2>
+
+
+ <?php 
+// if(false == empty($goukei) || false == empty($goukeifeed)){echo $user->username . ", ";}?>
+ <?php print $goukei; 
+// if(false == empty($goukei) && false == empty($goukeifeed)){echo " and ";}?>
+
+<?php
+if(false == empty($goukei) && false == empty($goukeifeed)){echo "on Daily Report!! ";}?>
+
+<!--<?php print $goukeifeed; ?> -->
+<!-- on your Daily Report !!-->
+
+</h2>
+</div>
