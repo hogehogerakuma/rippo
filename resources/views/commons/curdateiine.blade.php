cb8a0e7bbe4dc3f940190c1ae11103cbd876985f
hogehoge.tarou <hogehogerakumma@gmail.com>
2018-07-24 07:16:04 +0000

    curdate iine fixed

--- resources/views/commons/curdateiine.blade.php
+++ resources/views/commons/curdateiine.blade.php
@@ -10,7 +10,7 @@
  
  $favorited = DB::table('user_favorite')->join('reports', 'reports.id', '=', 'user_favorite.report_id')->whereDay('reports.created_at', $now_date)->where( 'reports.user_id', $user->id )->count();
 
-$popopo = App\Report::whereDate('created_at', DB::raw('CURDATE()'))->where('user_id', $user->id)->get();
+$popopo = App\Report::whereDate('created_at', DB::raw('current_date'))->where('user_id', $user->id)->get();
         
         if (isset ($popopo) && count($popopo)>0 ) {
 
