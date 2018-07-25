5bfbc3e2d79f8c62f76be8078745ab52be1bc71b
hogehoge.tarou <hogehogerakumma@gmail.com>
2018-07-25 05:05:13 +0000

    qooooo

--- app/Http/Controllers/UserCommentController.php
+++ app/Http/Controllers/UserCommentController.php
@@ -20,6 +20,7 @@ class UserCommentController extends Controller
         ->where('reports.user_id', $id)
         ->whereDate('reports.created_at' ,$thatday_date)
         ->first();
+        // dd($id); exit;
         $report = Report::find($report->id);
         $comments = $report->comments()->get();
         // $comments = [];
